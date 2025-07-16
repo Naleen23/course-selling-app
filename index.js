aa526857c81845fba0e60329e66f8d5e05f08ad3
Naleen23 <nalivegaming2312@gmail.com>
2025-07-16 12:42:15 +0530

    Step 2 Add skeleton user

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+app.post("user/signup", function(req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+} )
+
+app.post("/user/signin", function(req, res) {
+    res.json({
+        message: "signin endpoint"
+    })
+})
+
+app.get("/user/purchases", function(req, res) {
+    res.json({
+        message: "purchases endpoint"
+    })
+})
+
+app.post("/course/purchases", function(req, res){
+    res.json({
+        message: "create purchase endpoint"
+    })
+})
+
+
+app.get("/courses", function(req, res) {
+    res.json({
+        message: "courses endpoint"
+    })
+})
+
+app.listen(7777, function() {
+    console.log("server is running on port 7777");
+})
