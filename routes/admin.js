d4711dfdbac4bf30b74f2efd902deab62095ed74
Naleen23 <nalivegaming2312@gmail.com>
2025-07-16 14:43:54 +0530

    Step 5 ,added admin router

---
+++ routes/admin.js
@@ -0,0 +1,36 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", function(req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+adminRouter.post("/signin", function(req, res) {
+    res.json({
+        message: "signin endpoint"
+    })
+})
+
+adminRouter.post("/course", function(req, res) {
+    res.json({
+        message:"course endpoint"
+    })
+})
+
+adminRouter.put("/course", function(req, res) {
+    res.json({
+        message: "update course endpoint"
+    })
+})
+
+adminRouter.put("/course", function(req, res) {
+    res.json({
+        message: "update course"
+    })
+})
+
+module.exports = {
+    adminRouter: adminRouter
+}
